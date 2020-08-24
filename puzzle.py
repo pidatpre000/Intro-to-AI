c9e62a6552d60e816e3dbcbd33c2b9f3a8df02dc
pidatpre000 <pidatpre000@users.noreply.github.com>
2020-08-24 14:32:15 -0500

    automated commit by submit50

---
+++ puzzle.py
@@ -0,0 +1,105 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    Biconditional(AKnave, Not(AKnight)),
+    Biconditional(AKnight, Not(AKnave)),
+    #Basic Rules
+    Implication( AKnight, And( AKnight,AKnave ) ),
+    Implication(AKnave, Not( And(AKnight,AKnave) ) ),
+    Or(Biconditional (AKnight,BKnave), Biconditional( AKnave, BKnight))
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And( 
+    Biconditional(AKnave, Not(AKnight)),
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnave, Not(BKnight)),
+    Biconditional(BKnight, Not(BKnave)),
+    #Basic Rules
+
+    Implication( AKnight, And( AKnave, BKnave ) ),
+    Implication(AKnave, Not( And( AKnave, BKnave ) ) ),
+    Or(Biconditional (AKnight,BKnave), Biconditional( AKnave, BKnight))
+
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    Biconditional(AKnave, Not(AKnight)),
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnave, Not(BKnight)),
+    Biconditional(BKnight, Not(BKnave)),
+    Biconditional(CKnight,Not(CKnave)),
+    Biconditional(CKnave, Not(CKnight)),
+    #Basic Rules
+
+    Implication(AKnight, Or(And(AKnight,BKnight), And(AKnave,BKnave)) ),
+    Implication(AKnave, Or(And(AKnight,BKnave), And(AKnave,BKnight)) ),
+    Implication(BKnight, Or(And(AKnight,BKnave), And(AKnave,BKnight)) ),
+    Implication(AKnight, Or(And(AKnight,BKnight), And(AKnave,BKnave)) ),
+    Or(Biconditional (AKnight,BKnave), Biconditional( AKnave, BKnight))
+
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+
+    Biconditional(AKnave, Not(AKnight)),
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnave, Not(BKnight)),
+    Biconditional(BKnight, Not(BKnave)),
+    Biconditional(CKnave, Not(CKnight)),
+    Biconditional(CKnight, Not(CKnave)),
+    #basic rules
+
+    Implication(BKnight, CKnave),
+    Implication(BKnave, CKnight),
+    Implication(CKnight,AKnight),
+    Implication(CKnave,AKnave),
+    Implication(And(BKnight, AKnight), AKnave),
+    Implication(And(BKnight, AKnave), AKnight),
+    Implication(And(BKnave, AKnight), AKnight),
+    Implication(And(BKnave, AKnave), AKnave)    
+    )
+
+    
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
